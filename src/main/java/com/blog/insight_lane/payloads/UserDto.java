0d2702ee48e9b82f4bba541ecc1905985d84065e
sushoban <sushobhan712702@gmail.com>
2024-12-02 01:34:33 +0530

    Added bean Validation for user details

--- src/main/java/com/blog/insight_lane/payloads/UserDto.java
+++ src/main/java/com/blog/insight_lane/payloads/UserDto.java
@@ -1,5 +1,8 @@
 package com.blog.insight_lane.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +12,15 @@ import lombok.Setter;
 @Setter
 public class UserDto {
     private int id;
+
+    @NotEmpty
     private String name;
+
+    @Email(message = "Please provide a valid email address")
     private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
+
     private String about;
 }
