77c4edd636cc043146029e38bd06ed22a17c8eb0
sushoban <sushobhan712702@gmail.com>
2025-01-11 23:46:42 +0530

    Added Role Entity ✨

---
+++ src/main/java/com/blog/insight_lane/entities/Role.java
@@ -0,0 +1,18 @@
+package com.blog.insight_lane.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer roleId;
+    private String roleName;
+}
